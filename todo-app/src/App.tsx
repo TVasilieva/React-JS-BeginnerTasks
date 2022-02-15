a73fe563d001357e0b395e73a0e0e2cb559811f4
Tonya Vasilieva <tonya1997vasilieva@gmail.com>
2022-02-15 10:26:13 +0300

    feat: added bg-image

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from "react";
+import "./App.css";
 
 const App: FC = () => {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <h1>todo</h1>
+      </div>
+    </>
+  );
 };
 
 export default App;
